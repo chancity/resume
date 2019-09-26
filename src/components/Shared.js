d394ea4769a7847cfe2bf25f1e666abce04a37cb
Chancey Hawbaker <chancey.hawbaker@petco.com>
2019-09-26 09:07:25 -0700

    initial

--- src/components/Shared.js
+++ src/components/Shared.js
@@ -2,25 +2,19 @@ import styled from 'styled-components/macro'
 
 export const WrapperContainer = styled.div`
 	display: flex;
-    -webkit-box-align: center;
     align-items: center;
-    -webkit-box-pack: justify;
     justify-content: space-between;
 `;
 
 export const StartWrapper = styled.div`
 	display: flex;
-    -webkit-box-align: center;
     align-items: center;
-    -webkit-box-pack: start;
     justify-content: flex-start;
 `;
 
 export const EndWrapper = styled.div`
 	display: flex;
-    -webkit-box-align: center;
     align-items: center;
-    -webkit-box-pack: end;
     justify-content: flex-end;
 `;
 
