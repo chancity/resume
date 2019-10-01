099c0ce7e1a391d35a19b79bfbdc86e7b7067ef7
Chancey Hawbaker <chancey.hawbaker@petco.com>
2019-10-01 13:11:09 -0700

    removed padding and increase layout max width

--- src/components/layout/Body.js
+++ src/components/layout/Body.js
@@ -5,9 +5,10 @@ export const Body = styled.div`
 	display: flex;
 	flex-direction: column;
     width: 100%;
-    max-width: 960px;
+    max-width: 1024px;
     padding-left: 10px;
     padding-right: 10px;
+    padding: 0;
     margin: 0 auto;
     
     ${Section}:last-child{
