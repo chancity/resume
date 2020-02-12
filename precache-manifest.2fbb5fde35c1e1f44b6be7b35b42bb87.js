8924c4cb9fa5bb61741bf382ec1a7b407c048867
chancity <hawbakerchancey@gmail.com>
2020-02-11 22:33:49 -0800

    Updates

---
+++ precache-manifest.2fbb5fde35c1e1f44b6be7b35b42bb87.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ddeb61b7b4bd9d0e7650020c8b701d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1593c73cf9a12550191",
+    "url": "/static/css/main.0194bccb.chunk.css"
+  },
+  {
+    "revision": "b666d8eb1e00c173f775",
+    "url": "/static/js/2.b17c19f3.chunk.js"
+  },
+  {
+    "revision": "13853f2e19f3a42c16a1c69d7187f441",
+    "url": "/static/js/2.b17c19f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1593c73cf9a12550191",
+    "url": "/static/js/main.8c8935fc.chunk.js"
+  },
+  {
+    "revision": "04e1f75d6cf222abe257",
+    "url": "/static/js/runtime-main.fa53a738.js"
+  },
+  {
+    "revision": "d01a0622fa8e09ad34e058f490236b49",
+    "url": "/static/media/facebook.d01a0622.svg"
+  },
+  {
+    "revision": "4299de0fe85f56b9050f7a3ab2ac99c8",
+    "url": "/static/media/github.4299de0f.svg"
+  },
+  {
+    "revision": "d15c3e849bc249844e2901ad2495ddf1",
+    "url": "/static/media/instagram.d15c3e84.svg"
+  },
+  {
+    "revision": "ec68f4aa01b645b0d4c67d1771f80aa1",
+    "url": "/static/media/linkedin.ec68f4aa.svg"
+  },
+  {
+    "revision": "5bc700b06513381ef444dfcf9f7c0f26",
+    "url": "/static/media/me.5bc700b0.png"
+  },
+  {
+    "revision": "33512190f0b4a2006b517389d28f1135",
+    "url": "/static/media/school.33512190.svg"
+  },
+  {
+    "revision": "a8b0fb570ac8f2da54a43762ebd0860d",
+    "url": "/static/media/work.a8b0fb57.svg"
+  }
+]);
